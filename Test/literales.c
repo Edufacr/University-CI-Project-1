c3de93ab8110ba53b153056aa42b9c40957904ad
Eduardo <edumadrim@gmail.com>
2021-05-12 11:20:06 -0600

    Official Tests added

--- Test/literales.c
+++ Test/literales.c
@@ -1,25 +1,21 @@
-//Numeros enteros
-
-0000 // Debe tomar 4 ceros
-11U
--1
-+2
-010
-011L
-0x1F
-0x2AUL
-
-// ** Debe aceptar todos excepto el "-" y "+" cómo literales.
-// ** El "-" y "+" se toman como operadores
-
-// Errores Numeros Enteros
-x2
-//Flotantes
-1.12
-.20
-0.2E20
-10E-10
-
-//Caracteres
-
-//Strings
+"Este es un String "
+'\0'
+'H','o','l','a','\0'
+123
+01234567
+0XAA20F243
++01234567
+-0XAA20F243
+1.23f
+123.345
+123E10
+123.22E10
+.22E10
+.E10   //se separara en OPERADOR ., ID E y numero 10 o error
++1.23f
+-123.345
+-123E10
+0XAA20FGGG  //se espera que separe el hexadecimal del IDENTIFICADOR GGG
+123r  //error o separa el numero del IDENTIFICADOR r
+ID123  //se espera que se clasifique como un ID
+014255689  //se espera que no se acepte como octal y de un error
