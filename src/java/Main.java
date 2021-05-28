02734c9b8f8f52994ceb8a37a157b5acc466bd8b
Eduardo <edumadrim@gmail.com>
2021-05-28 11:05:01 -0600

    Reorder code for ANT use

--- src/java/Main.java
+++ src/java/Main.java
@@ -4,7 +4,8 @@ import java.io.FileReader;
 class Main {
     public static void main(String[] args) throws FileNotFoundException {
         String fileName = "input.c";
-        parser p = new parser(new scanner(new FileReader(fileName)));
+        Parser p = new Parser(new Scanner(new FileReader(fileName)));
+        
         try {
             //p.parse();
             System.out.println(p.debug_parse().value);
