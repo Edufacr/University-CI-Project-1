1e9b403bf1c5ecfea803e87a968c6c7440a8e0ed
Eduardo <edumadrim@gmail.com>
2021-06-03 22:20:41 -0600

    while & for added

--- Test/cup.c
+++ Test/cup.c
@@ -21,3 +21,24 @@ void foo(int p1, char p2){
         }
     }
 }
+
+int foo2 () {
+    break;
+    continue;
+    return 1+2;
+}
+
+void foo3() {
+    int i;
+    while (0) {
+        a = 3;
+    }
+    for (i = 0; i + 100; i = i + 1) {
+        b = 3;
+    }
+    while (1) {
+       while (1) {
+           c = 0; 
+       } 
+    }
+}
